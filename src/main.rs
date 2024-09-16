feaa6ad825ad3628ea580f49a94a1f98cfc4caab
0xlay <zeroxlay@protonmail.com>
2024-09-16 19:58:24 +0300

    ✨feat: add support for the --buffer argument

--- src/main.rs
+++ src/main.rs
@@ -7,40 +7,49 @@ mod config;
 mod hasher;
 
 use clap::Parser;
-use hasher::Hash;
-
-// TODO: add vscode workspace with plugins
 
 fn main() {
     let args = config::Args::parse();
+
     match args.algorithm {
-        config::Algorithm::Md5 { path } => {
-            let mut md5 = hasher::Md5Hash::new();
-            match md5.generate_from_file(path.as_path()) {
-                Ok(hash) => println!("MD5 ({}) = {}", path.to_str().unwrap_or_default(), hash),
-                Err(e) => eprintln!("Failed to generate hash! Error: {e}"),
-            }
+        config::Algorithm::Md5 { path, buffer } => {
+            process_hash(hasher::Md5Hash::new(), path, buffer, "MD5");
         }
-        config::Algorithm::Sha1 { path } => {
-            let mut sha1 = hasher::Sha1Hash::new();
-            match sha1.generate_from_file(path.as_path()) {
-                Ok(hash) => println!("SHA1 ({}) = {}", path.to_str().unwrap_or_default(), hash),
-                Err(e) => eprintln!("Failed to generate hash! Error: {e}"),
-            }
+        config::Algorithm::Sha1 { path, buffer } => {
+            process_hash(hasher::Sha1Hash::new(), path, buffer, "SHA1");
         }
-        config::Algorithm::Sha256 { path } => {
-            let mut sha256 = hasher::Sha256Hash::new();
-            match sha256.generate_from_file(path.as_path()) {
-                Ok(hash) => println!("SHA256 ({}) = {}", path.to_str().unwrap_or_default(), hash),
-                Err(e) => eprintln!("Failed to generate hash! Error: {e}"),
-            }
+        config::Algorithm::Sha256 { path, buffer } => {
+            process_hash(hasher::Sha256Hash::new(), path, buffer, "SHA256");
         }
-        config::Algorithm::Sha512 { path } => {
-            let mut sha512 = hasher::Sha512Hash::new();
-            match sha512.generate_from_file(path.as_path()) {
-                Ok(hash) => println!("SHA512 ({}) = {}", path.to_str().unwrap_or_default(), hash),
-                Err(e) => eprintln!("Failed to generate hash! Error: {e}"),
-            }
+        config::Algorithm::Sha512 { path, buffer } => {
+            process_hash(hasher::Sha512Hash::new(), path, buffer, "SHA512");
         }
+    }
+}
+
+fn process_hash<H: hasher::Hash>(
+    mut hasher: H,
+    path: Option<std::path::PathBuf>,
+    buffer: Option<String>,
+    algorithm_name: &str,
+) {
+    if let Some(path) = path {
+        match hasher.generate_from_file(path.as_path()) {
+            Ok(hash) => println!(
+                "{} hash ({}) = {}",
+                algorithm_name,
+                path.to_str().unwrap_or_default(),
+                hash
+            ),
+            Err(e) => eprintln!("Failed to generate {algorithm_name} hash! Error: {e}"),
+        }
+    } else if let Some(buffer) = buffer {
+        println!(
+            "{} hash = {}",
+            algorithm_name,
+            hasher.generate_from_buf(buffer.as_bytes())
+        );
+    } else {
+        eprintln!("No input provided for {algorithm_name} hash generation!");
     }
 }
